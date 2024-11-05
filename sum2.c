8b9721ecdf6e82d07e478c3ae191906f24df5271
bindujojo626 <bindujojo626@gmail.com>
2024-11-05 08:11:55 +0530

    Update sum2.c

--- sum2.c
+++ sum2.c
@@ -7,7 +7,8 @@ sum2number() {
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
-    sum = number1 + number2;      
+    sum = number1 + number2;
+    
     
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
